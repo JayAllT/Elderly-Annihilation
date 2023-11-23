ed32c8dc0b66226cb5c81f900c1f30bb8d51a0b5
WhyDoLogic <84210614+WhyDoLogic@users.noreply.github.com>
2023-11-23 15:08:53 +1100

    Cursor update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
