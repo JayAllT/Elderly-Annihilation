35f5d1e574772ac924f2a30ba6faac431b41fc5a
WhyDoLogic <roguegamerz27@gmail.com>
2023-11-28 14:05:12 +1100

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,47 @@ public class PlayerMovement : MonoBehaviour
     //Variables
     public GameObject player;
     public float speed;
-
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
+    public bool canRoll;
+    public float rollTime;
+    public float canWalk;
+
+
     void Start()
     {
+        //Variables
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
 
-}
+    }
 
-    // Update is called once per frame
+   
     void Update()
     {
-
+        //Basic Player Movement
         float verticalMovement = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
         float horizontalMovement = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
 
-        player.transform.Translate(new Vector3(horizontalMovement, verticalMovement, 0));
+        if (canWalk = true)
+        {
+            player.transform.Translate(new Vector3(horizontalMovement, verticalMovement, 0));
+        }
+        //Roll Mechanic
+
+        if (Input.GetMouseButtonDown(1) && canRoll)
+        {
+            var mousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+            canWalk = false; 
+
+            player.transform.Translate(direction)
+        }
+
+
     }
+
+
+
 }
